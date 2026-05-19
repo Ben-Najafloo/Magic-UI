f9216d6b8dfe25784f3deac56d607e9a94b947c8
Ben Najafloo <ben@zerolatency.tv>
2026-05-19 08:53:47 +0200

    arash dark mode added2

--- src/app/(extra)/arashAzmi/page.jsx
+++ src/app/(extra)/arashAzmi/page.jsx
@@ -536,7 +536,7 @@ export default function Project2Page() {
 
                     <section className="py-28">
                         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                            <h2 className="text-3xl font-bold text-gray-900 mb-8">Featured Courses</h2>
+                            <h2 className="text-3xl font-bold text-gray-900 dark:text-gray-200 mb-8">Featured Courses</h2>
                             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                                 {courses.map((course) => (
                                     <Card

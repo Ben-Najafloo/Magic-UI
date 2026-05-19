6e1d551b7d40f4eb24ed6e6c30cc8f18435e4044
Ben Najafloo <ben@zerolatency.tv>
2026-05-19 08:08:52 +0200

    arash header changed

--- src/app/(extra)/arashAzmi/page.jsx
+++ src/app/(extra)/arashAzmi/page.jsx
@@ -380,7 +380,7 @@ export default function Project2Page() {
     return (
         <>
             {/* Header */}
-            <header className="sticky top-0 z-50 bg-white border-b border-gray-200">
+            <header className="sticky top-0 z-50 border-b border-gray-200">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
                     <div className="flex justify-between items-center py-4">
